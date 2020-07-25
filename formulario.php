7927d7b2ab0cb06d9f6c75ee23dd251110f749ab
Davi Patiño da Silva <et48111@brsti003506.brscan.com.br>
2020-07-24 23:31:48 -0300

    Commit Inicial

---
+++ formulario.php
@@ -0,0 +1,83 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<h2>Cadastro</h2>
+
+<form action="#" method="post">
+    <div class="form-row">
+        <div class="form-group col-md-8">
+            <label for="nome">Nome</label>
+            <input type="text" 
+                class="form-control>"
+                id="nome" name="nome" placeholder="Nome">
+        </div>
+
+        <div class="form-group col-md-4">
+            <label for="nascimento">Nascimento</label>
+            <input type="text"
+                class="form-control>"
+                id="nascimento" name="nascimento"
+                placeholder="Nascimento">
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <label for="email">E-mail</label>
+            <input type="text"
+                class="form-control>"
+                id="email" name="email" placeholder="E-mail">        
+        </div>
+
+    <div class="form-check">
+      <p>sexo</p>
+            <input class="form-check-input" type="radio" name="exampleRadios" id="sexo" value="option1" checked>
+            <label class="form-check-label" for="exampleRadios1">
+                Masculino
+            </label>
+    </div>
+    <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">
+                Feminino
+            </label>
+</div>
+    </div>
+
+        <div class="form-group col-md-12">
+            <label for="salario">Salário</label>
+            <input type="text"
+                class="form-control>"
+                id="salario" name="salario"
+                placeholder="Salário">
+        </div>
+    </div>
+
+    <button class="btn btn-primary btn-lg">Enviar</button>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
